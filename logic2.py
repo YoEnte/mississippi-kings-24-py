630dd7f54294fb78d1f379c01d45f8babeefabef
YoEnte <anthonyroeder@live.de>
2024-06-04 00:30:27 +0200

    added udp, scores, docks etc

--- logic2.py
+++ logic2.py
@@ -32,6 +32,9 @@ from socha.starter import Starter
 
 import networkx as nx
 import math
+import json
+import socket
+from datetime import datetime
 
 ## fields   -> SoCha Fields
 ## nodes    -> graph key
@@ -41,12 +44,9 @@ import math
 ## step     -> single steps needed
 ## distance -> dtistance (can have evals etc)
 
-## ToDo List:
-## Read from graph.txt
-## Autoread from graph.txt
-## start both scripts at same time
-## delay logic2
-## finish redoing
+class MyEncoder(json.JSONEncoder):
+    def default(self, o):
+        return str(o).replace('CubeDirection::', '').replace('FieldType.', '').replace('TeamEnum.', '')
 
 class Logic(IClientHandler):
     game_state: GameState
@@ -57,6 +57,7 @@ class Logic(IClientHandler):
         self.maxSegments = 0
         self.me: Ship
         self.infinite = 999999999999
+        self.startTimeString = f"{datetime.now():%Y-%m-%dT%H:%M:%SM%f}"[:-3]
         
         self.directionList = [                 # Right -> Clockwise
             CubeDirection.Right,
@@ -75,7 +76,7 @@ class Logic(IClientHandler):
             CubeCoordinates(1, -1)  #0
         ]
 
-        open('graph.txt', 'w').close()
+        self.needToBeDock = []
 
     def buildGraph(self, segmentIndex):
         segment = self.game_state.board.segments[segmentIndex]
@@ -89,17 +90,35 @@ class Logic(IClientHandler):
 
             # add column of cubes to graph
             for cube, row in cubes:
-
                 node = ';'.join(str(x) for x in cube.coordinates())
                 hasStream = self.game_state.board.does_field_have_stream(cube)
                 field_type = segment.fields[col][row].field_type
 
+                if field_type == FieldType.Passenger:
+                    passengerDirection = segment.fields[col][row].passenger.direction
+                    dockCube = cube.plus(passengerDirection.vector())
+                    self.needToBeDock.append([';'.join(str(x) for x in dockCube.coordinates()), False])
+
+                # add nodes
                 self.G.add_node(
                     node,
                     fieldType=field_type,
                     hasStream=hasStream,
-                    segment=segmentIndex
+                    segment=segmentIndex,
+                    dock=False
                 )
+                
+                # add docks
+                for d in self.needToBeDock:
+                    if d[1] == True:
+                        continue
+
+                    try:
+                        if self.G.nodes[d[0]]['fieldType'] == FieldType.Water or self.G.nodes[d[0]]['fieldType'] == FieldType.Goal:
+                            self.G.nodes[d[0]]['dock'] = True
+                            d[1] = True
+                    except:
+                        pass
 
             base = base.plus(segment.direction.vector())
 
@@ -115,17 +134,17 @@ class Logic(IClientHandler):
             },
             'start': {
                 'startNodes': [],
-                'startDirections': [CubeDirection.Right],
+                'startDirections': [CubeDirection.Left],
                 'rotationMulti': 2,
                 'startInt': 0,
-                'mirroredDirections': True
+                'mirroredDirections': False
             },
             'me': {
                 'startNodes': [';'.join(str(x) for x in self.me.position.coordinates())],
-                'startDirections': [self.me.direction],
+                'startDirections': [self.me.direction.opposite()],
                 'rotationMulti': 2,
                 'startInt': 0,
-                'mirroredDirections': True
+                'mirroredDirections': False
             }
         }
 
@@ -138,7 +157,6 @@ class Logic(IClientHandler):
         cubes += [[lastCenter.plus(self.game_state.board.next_direction.rotated_by(2).vector().times(d + 1)), d + 3] for d in range(2)]
 
         for cube, row in cubes:
-
             node = ';'.join(str(x) for x in cube.coordinates())
             if self.G.nodes[node]['fieldType'] == FieldType.Water or self.G.nodes[node]['fieldType'] == FieldType.Goal:
                 if self.maxSegments < 8 or row >= 1 or row <= 3: # if no goal on map or goal on map and field is goal else: goal on map field is no goal and dijkstra magic
@@ -213,43 +231,75 @@ class Logic(IClientHandler):
 
                             newWeight = (self.G.nodes[smallestNode][distanceTag] + 1) * (abs(flow.turn_count_to(self.G.nodes[smallestNode][directionTag])) + 1)
 
-                            if self.G.nodes[neighborNode][distanceTag] > newWeight:
+                            if newWeight < self.G.nodes[neighborNode][distanceTag]:
                                 #print("relaxed to", self.G.nodes[smallestNode]['distance'] + 1)
                                 self.G.nodes[neighborNode][distanceTag] = newWeight
                                 self.G.nodes[neighborNode][directionTag] = flow
 
 
-                            # neighbors unvisted machen wenn
+                            # neighbors unvisted machen wenn relax???
 
                     except:
                         #print("out of bounds")
                         continue
+
+    def calcScores(self):
+
+        for n in self.G.nodes:
+            self.G.nodes[n]['distanceScore']    = self.G.nodes[n]['startDistance'] / (self.G.nodes[n]['meDistance'] + self.G.nodes[n]['nextDistance'])
+            if self.G.nodes[n]['dock'] == False:
+                self.G.nodes[n]['dockScore']    = self.G.nodes[n]['startDistance'] / (self.G.nodes[n]['meDistance'] + self.G.nodes[n]['nextDistance'])
+            else:
+                self.G.nodes[n]['dockScore']    = self.G.nodes[n]['startDistance'] / ((self.G.nodes[n]['meDistance'] / 10 ) + self.G.nodes[n]['nextDistance'])
+
+        self.sortedGraph        = sorted(self.G.nodes.data(), key=lambda x: (-x[1]['distanceScore'], x[1]['meDistance']))[0:10]
+        self.sortedGraphDock    = sorted(self.G.nodes.data(), key=lambda x: (-x[1]['dockScore'], x[1]['meDistance']))[0:10]
 
     def randomMove(self):
         
         possible_moves: List[Move] = self.game_state.possible_moves()
         return possible_moves[random.randint(0, len(possible_moves) - 1)]
 
-    def printGraph(self, console, useLogging, writeFile):
-        graphstr = str(self.G.nodes.data()).replace('::', '.')
+    def printGraph(self, console, useLogging, sendToServer):
+        self.turnData = {
+            "turn": self.game_state.turn,
+            "player": self.game_state.current_ship.team,
+            "time": self.startTimeString,
+            "currentShip": {
+                "coords": ';'.join(str(x) for x in self.game_state.current_ship.position.coordinates())
+            },
+            "otherShip": {
+                "coords": ';'.join(str(x) for x in self.game_state.other_ship.position.coordinates())
+            },
+            "data": list(self.G.nodes.data()),
+            "sortedGraph": self.sortedGraph,
+            "sortedGraphDock": self.sortedGraphDock,
+        }
+        self.turnString = json.dumps(self.turnData, cls=MyEncoder)
 
         if console:
             if useLogging:
-                logging.info(graphstr)
+                logging.info(self.turnString)
+                logging.info("\n\n")
             else:
-                print(graphstr)
+                print(self.turnString)
+                print("\n\n")
 
-        if writeFile:
-            with open("graph.txt", "a") as file:
-                file.writelines(["turn: ", str(self.game_state.turn), "\n"])
-                file.write(graphstr)
-                file.write("\n\n")
+        if sendToServer:
+            self.sendUDP('192.168.178.234', 30001)
+        
+
+    def sendUDP(self, host, port):
+
+        turnBytes = str.encode(self.turnString)
+        server = (host, port)
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        UDPClientSocket.sendto(turnBytes, server)
 
     def calculate_move(self) -> Move:
         # basic stuff
-        logging.info("\n\n\n\n")
+        logging.info("\n\n")
         logging.info("Calculate move...")
-        print('turn:', self.game_state.turn)
 
         # set turn variables
         self.me = self.game_state.current_ship
@@ -264,8 +314,9 @@ class Logic(IClientHandler):
             self.segmentsAdded += 1
 
         self.setDistances(self.setStarts())
+        self.calcScores()
 
-        self.printGraph(console=False, useLogging=True, writeFile=True)
+        self.printGraph(console=False, useLogging=False, sendToServer=True)
 
         # send move        
         #move = self.randomMove()
@@ -274,7 +325,6 @@ class Logic(IClientHandler):
 
     def on_update(self, state: GameState):
         self.game_state = state
-
 
 if __name__ == "__main__":
     Starter(logic=Logic())
